5c9f8506350076a702c9027ee83df9f2ddcc8efa
Erik O'Shaughnessy <erik.oshaughnessy@oracle.com>
2015-04-16 09:29:56 -0500

    Initial Commit

---
+++ StatusThing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StatusThing
+//
+//  Created by Erik on 4/16/15.
+//  Copyright (c) 2015 Symbolic Armageddon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ StatusThing/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  StatusThing
+//
+//  Created by Erik on 4/16/15.
+//  Copyright (c) 2015 Symbolic Armageddon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
