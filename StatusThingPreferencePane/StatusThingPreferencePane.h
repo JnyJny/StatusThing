50485b8eb55a938ae54ec129c6b45762358b67ec
Erik O'Shaughnessy <erik.oshaughnessy@oracle.com>
2015-05-14 09:29:44 -0500

    Revert "Re-un-changed my mind, NSPreferencePane 86ed until I have time to figure it out."
    This reverts commit af77e30d2d26a999db9681277231cae1acacfefe.

---
+++ StatusThingPreferencePane/StatusThingPreferencePane.h
@@ -0,0 +1,15 @@
+//
+//  StatusThingPreferencePane.h
+//  StatusThingPreferencePane
+//
+//  Created by Erik on 5/13/15.
+//  Copyright (c) 2015 Symbolic Armageddon. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface StatusThingPreferencePane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ StatusThingPreferencePane/StatusThingPreferencePane.m
@@ -0,0 +1,17 @@
+//
+//  StatusThingPreferencePane.m
+//  StatusThingPreferencePane
+//
+//  Created by Erik on 5/13/15.
+//  Copyright (c) 2015 Symbolic Armageddon. All rights reserved.
+//
+
+#import "StatusThingPreferencePane.h"
+
+@implementation StatusThingPreferencePane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
